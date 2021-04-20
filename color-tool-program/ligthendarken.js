887ae9b29def3b1be986b5cec8ed0faa3665e635
Luciana Terrazas <luciana.cterrazas@gmail.com>
2021-04-20 16:52:39 +0000

    Add toggle function

--- color-tool-program/ligthendarken.js
+++ color-tool-program/ligthendarken.js
@@ -4,6 +4,9 @@ const alteredColor = document.getElementById('alteredColor');
 const alteredColorText = document.getElementById('alteredColorText');
 const slider = document.getElementById("slider");
 const sliderText = document.getElementById("sliderText");
+const lightenText = document.getElementById('lightenText');
+const darkenText = document.getElementById('darkenText');
+const toggleBtn = document.getElementById('toggleBtn');
 
 //Check to see whether the input from the user is a valid
 //hex color
@@ -139,3 +142,15 @@ slider.addEventListener('input', () => {
 // Next steps:
 //lightenText, darkenText, toggleBtn
 //click event listener to the toggle btn
+toggleBtn.addEventListener('click', () => {
+  if(toggleBtn.classList.contains('toggled')){
+    toggleBtn.classList.remove('toggled');
+    lightenText.classList.remove('unselected');
+    darkenText.classList.add('unselected');
+  } else {
+    toggleBtn.classList.add('toggled');
+    lightenText.classList.add('unselected');
+    darkenText.classList.remove('unselected');
+  } 
+})
+
