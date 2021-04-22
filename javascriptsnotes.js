bdce2b6aebf0ca568c671e7cfc56e818ba36294d
Luciana Terrazas <luciana.cterrazas@gmail.com>
2021-04-22 18:51:17 +0000

    Add all files back

---
+++ javascriptsnotes.js
@@ -0,0 +1,126 @@
+// JS studies from developer.mozilla.org
+
+// Method .startsWith()
+// lets you determine whether or not a string begins determined string
+// this method is case-sensitive
+// str.startsWith(searchString[, position])
+
+let str = 'To be, or not to be, that is the question.'
+
+console.log(str.startsWith('To be'))          // true
+console.log(str.startsWith('not to be'))      // false
+console.log(str.startsWith('not to be', 10))  // true
+
+// ------------------------------------------------------------------
+
+// Method substring()
+// extracts characters from indexStart up to but not including indexEnd
+// str.substring(indexStart[, indexEnd])
+//
+// If indexStart is greater than indexEnd, then the effect 
+// [ of substring() is as if the two arguments were swapped.
+//
+// Any argument value that is less than 0 or greater than stringName.length 
+// [ is treated as if it were 0 and stringName.length, respectively.
+
+let anyString = 'Mozilla'
+
+// Displays 'M'
+console.log(anyString.substring(0, 1))
+console.log(anyString.substring(1, 0))
+
+// Displays 'Mozill'
+console.log(anyString.substring(0, 6))
+
+// Displays 'lla'
+console.log(anyString.substring(4))
+console.log(anyString.substring(4, 7))
+console.log(anyString.substring(7, 4))
+
+// Displays 'Mozilla'
+console.log(anyString.substring(0, 7))
+console.log(anyString.substring(0, 10))
+
+// Displays 'illa' the last 4 characters
+let anyString = 'Mozilla'
+let anyString4 = anyString.substring(anyString.length - 4)
+console.log(anyString4)
+
+// Displays 'zilla' the last 5 characters
+let anyString = 'Mozilla'
+let anyString5 = anyString.substring(anyString.length - 5)
+console.log(anyString5)
+
+// ------------------------------------------------------------------
+
+// The slice() method returns an empty string if
+// [ indexStart is greater than indexEnd.
+//
+// When it is given negative values it counts backwards
+// [ from the end of the string to find the indexes.
+
+let text = 'Mozilla'
+console.log(text.substring(5, 2))  // => "zil"
+console.log(text.slice(5, 2))      // => ""
+
+console.log(text.slice(-5, 2))   // => ""
+console.log(text.slice(-5, -2))  // => "zil"
+
+// ------------------------------------------------------------------
+
+// Method .replace()
+// returns a new string with some or all matches
+// [ of a pattern replaced by a replacement
+// const newStr = str.replace(regexp|substr, newSubstr|function)
+
+const p = 'The quick brown fox jumps over the lazy dog. \
+            If the dog reacted, was it really lazy?';
+
+console.log(p.replace('dog', 'monkey'));
+
+const regex = /Dog/i;
+console.log(p.replace(regex, 'ferret'));
+
+// ------------------------------------------------------------------
+
+// String property .lenght:
+const str = 'Life, the universe and everything. Answer:';
+console.log(`${str} ${str.length}`);
+// "Life, the universe and everything. Answer: 42"
+
+// ------------------------------------------------------------------
+
+// The parseInt() function:
+// converts its first argument to a string, parses that string, then returns an integer or NaN:
+// parseInt(string [, radix])
+
+const x = 5;
+const y = "10";
+console.log(x + y) // => 510
+const yInt = Number.parseInt(y);
+console.log(x + y) // => 15
+const yInt = Number.parseInt("10.9283 is my favorite number");
+console.log(x + y) // => 15
+// hint: use parseFloat if you want to preserver the decimal values
+
+// Converting decimal numbers to binary, octal and hexadecimal:
+
+// from different bases to decimal:
+const x = 5;
+console.log(number.parseInt(x, 2))  // => 3
+console.log(number.parseInt(x, 8))  // => 9
+console.log(number.parseInt(x, 16)) // => 17
+
+// from decimal to different bases:
+const x = Number("5");
+console.log("Decimal to binary:", x.toString(2));   // => 1011
+console.log("Decimal to binary:", x.toString(8));   // => 13
+console.log("Decimal to binary:", x.toString(16));  // => b
+
+// ------------------------------------------------------------------
+
+
+//.value
+//style.background
+//getElementById
+//addEventListener
---
+++ ligthendarken.js
@@ -0,0 +1,156 @@
+const hexInput = document.getElementById('hexInput');
+const inputColor = document.getElementById('inputColor');
+const alteredColor = document.getElementById('alteredColor');
+const alteredColorText = document.getElementById('alteredColorText');
+const slider = document.getElementById('slider');
+const sliderText = document.getElementById('sliderText');
+const lightenText = document.getElementById('lightenText');
+const darkenText = document.getElementById('darkenText');
+const toggleBtn = document.getElementById('toggleBtn');
+
+
+//Check to see whether the input from the user is 
+//a valid hex color
+//1. #000000 or 000000
+//2. check the length - should be either 3 or 6 characters
+const isValidHex = (hex) => {
+    if(!hex) return false;
+    const strippedHex = hex.replace('#', '');
+    return strippedHex.length === 3 || strippedHex.length === 6;
+}
+
+
+//Create a keyup event handler for hexInput
+//Check if hex color is valid
+//If hex color is valid, update the background color of inputColor 
+hexInput.addEventListener('keyup', () => {
+    const hex = hexInput.value; 
+    if(!isValidHex(hex)) return;
+    const strippedHex = hex.replace('#', '');
+    inputColor.style.backgroundColor = "#" + strippedHex;
+    reset(); // ? 
+})
+
+
+//Create a function to convert Hex to RGB
+//this should work with 3 or 6 character hex values
+//Hint - useParseInt(16) to convert a hex value to a decimal value
+//should return an object with 3 properties - r,g, and b
+//Test your function with a few different use cases
+const convertHexToRGB = (hex) => {
+    if(!isValidHex(hex)) return null;
+
+    let strippedHex = hex.replace('#','');
+    
+    if(strippedHex.length === 3) {
+      strippedHex = 
+        strippedHex[0] + strippedHex[0]
+      + strippedHex[1] + strippedHex[1]
+      + strippedHex[2] + strippedHex[2];
+    }
+    
+    const r  = parseInt(strippedHex.substring(0,2), 16);
+    const g  = parseInt(strippedHex.substring(2,4), 16);
+    const b  = parseInt(strippedHex.substring(4,6), 16);
+    
+    return {r,g,b}
+}
+  // console.log(convertHexToRGB("000"));
+
+
+//Create the function converRGBToHex
+//take in 3 parameters - r,g, and b
+//for each (r,g,b) - create a hex pair that is two characters long
+//return hex value starting with a hashtag
+//example - r.toString(16)
+const convertRGBToHex = (r, g, b) => {
+    const hexR = ('0' + r.toString(16)).slice(-2);
+    const hexG = ('0' + g.toString(16)).slice(-2);
+    const hexB = ('0' + b.toString(16)).slice(-2);
+
+    const hexValue = '#' + hexR + hexG + hexB;
+    return hexValue
+
+}
+// console.log(convertRGBToHex(255, 255, 255));
+
+
+// We need to add some constraints in our previous hex values
+// since hex values shouldn't be less than 0 or more than 255
+const increaseWithin0To255 = (hex, amount) => {
+    const newHex = hex + amount;
+    if(newHex > 255) return 255;
+    if(newHex < 0) return 0;
+    return newHex;
+    // return Math.min(255, Math.max(0, hex + amount));
+  }
+  
+
+//Create the alterColor function which accepts hex value and percentage
+//convert the hex value to rgb
+//increase each r,g,b value by appropriate amount (percentage of 255)
+//use the new r,g,b values to convert to a hex value
+//return the hex value
+const alterColor = (hex, percentage) => {
+    const {r,g,b} = convertHexToRGB(hex);
+    
+    const amount = Math.floor((percentage/100) * 255);
+    
+    const newR = increaseWithin0To255(r, amount);
+    const newG = increaseWithin0To255(g, amount);
+    const newB = increaseWithin0To255(b, amount);
+    console.log(newR, newG, newB)
+    return convertRGBToHex(newR, newG, newB);
+  }
+// console.log(alterColor('ccc', 10));
+
+
+// Now let's put things together:
+slider.addEventListener('input', () => {
+  
+    //check if hex is valid
+    if(!isValidHex(hexInput.value)) return;
+    //calulcate the appropriate value for the color alteration
+    //between positive and negative
+    const valueAddition  = 
+    toggleBtn.classList.contains('toggled') ? 
+    -slider.value 
+    : slider.value;
+    sliderText.textContent = `${slider.value}%`;
+    //get the altered hex value
+    const alteredHex = alterColor(hexInput.value, valueAddition);
+    //update the altered color
+    alteredColor.style.backgroundColor = alteredHex;
+    alteredColorText.innerText = `Altered Color ${alteredHex}`; 
+  })
+
+
+// Next steps:
+//lightenText, darkenText, toggleBtn
+//click event listener to the toggle btn
+toggleBtn.addEventListener('click', () => {
+  if(toggleBtn.classList.contains('toggled')){
+    toggleBtn.classList.remove('toggled');
+    lightenText.classList.remove('unselected');
+    darkenText.classList.add('unselected');
+  } else {
+    toggleBtn.classList.add('toggled');
+    lightenText.classList.add('unselected');
+    darkenText.classList.remove('unselected');
+  } 
+  reset(); 
+})
+
+
+//Set slider value to 0 and slider text to 0%
+//Set altered color to original input color
+//Reset alteredColorText to original input
+// call reset in toggleBtn click handler 
+// call reset in hexInput keyup handler 
+const reset = () =>{ 
+  slider.value = 0;
+  sliderText.innerText=`0%`;
+  alteredColor.style.backgroundColor = hexInput.value;
+  alteredColorText.innerText = `Altered Color ${hexInput.value}`; 
+
+}
